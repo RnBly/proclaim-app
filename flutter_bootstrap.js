0af606c0743d617ddbfe002f427c37b5f64e8434
RnBly <RnBly@users.noreply.github.com>
2025-10-23 22:46:17 +0000

    deploy: 1b07c5cbf73b97fb989821da34c49ac2dcc59dc7

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2664781586"
+    serviceWorkerVersion: "3548169287"
   }
 });
