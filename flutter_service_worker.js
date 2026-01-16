e80a4ab36c2af6d040b7f17b4a6dadeb176819a6
RnBly <RnBly@users.noreply.github.com>
2026-01-16 03:24:52 +0000

    deploy: d6395679c1eac85dc976bc83ac11c5d603a20c32

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -10,8 +10,8 @@ const RESOURCES = {"flutter.js": "f393d3c16b631f36852323de8e583132",
 "icons/Icon-maskable-192.png": "281ee10d5cee3871b5a387b0bdc3c080",
 "kakao_login.js": "07414cacc773f442bac5641fec6445a3",
 "manifest.json": "5926c3e8dc52bd152a14be5a0112b103",
-"index.html": "e91a15353ef468d30b4606e959f1a0f0",
-"/": "e91a15353ef468d30b4606e959f1a0f0",
+"index.html": "5ac1cd3be32c1c7e395e5433265e96fe",
+"/": "5ac1cd3be32c1c7e395e5433265e96fe",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
 "assets/AssetManifest.bin.json": "6db17a226f5c195f1c1ac875b7d12f13",
 "assets/assets/images/shofar_logo.png": "3238e1fc2f0f0fbe3e668b5a20f1ad75",
@@ -34,9 +34,9 @@ const RESOURCES = {"flutter.js": "f393d3c16b631f36852323de8e583132",
 "canvaskit/canvaskit.js.symbols": "48c83a2ce573d9692e8d970e288d75f7",
 "canvaskit/skwasm.js.symbols": "262f4827a1317abb59d71d6c587a93e2",
 "favicon.png": "c6f163e05123a399351fa07dfa6c8fe6",
-"flutter_bootstrap.js": "5932a4aa824bb0ae64f9dae8f00e238a",
+"flutter_bootstrap.js": "7a640103c5b42f7ec0a977db13126d16",
 "version.json": "a57506586b72d36422f35651deee7501",
-"main.dart.js": "ae3e139a176794b4a8a7ff25636e8b19"};
+"main.dart.js": "6221456b5727b6f6c105b0afc3277891"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
